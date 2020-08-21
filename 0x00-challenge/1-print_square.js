254a5c2967cb8051c74aea1f24d9f469b2b9f54a
ffelipegupe <1560@holbertonschool.com>
2020-08-21 00:22:19 +0000

    Add: adding var

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -13,7 +13,7 @@ if (process.argv.length <= 2) {
     process.exit(1);
 }
 
-size = process.argv[2];
+var size = process.argv[2];
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
